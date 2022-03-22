99ebebcdba1f91e3b85702c7a25a2a03b1ff6760
middlefitting <wjdtmdcjf2@gmail.com>
2022-03-22 17:52:10 +0900

    commitTest

--- src/main/java/com/studywithus/domain/member/Member.java
+++ src/main/java/com/studywithus/domain/member/Member.java
@@ -1,17 +1,19 @@
 package com.studywithus.domain.member;
 
 import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
-@Getter
+@Getter @Setter
 public class Member {
     @Id
     @GeneratedValue
-    @Column(name = "member_id")
+    @Column(name = "mem_id")
     private Long id;
 
     @Column(unique = true)
---
+++ src/main/java/com/studywithus/repository/member/MemberRepository.java
@@ -0,0 +1,4 @@
+package com.studywithus.repository.member;
+
+public class MemberRepository {
+}
