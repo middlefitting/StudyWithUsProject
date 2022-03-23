391f8f288a8ff73c2c2227c2cbafde3c6e1938c3
uk <uk@naver.com>
2022-03-23 18:03:19 +0900

    STYLE: Revise 'domain.board', 'domain.study', 'repository.board', 'repository.study'

--- src/main/java/com/studywithus/repository/board/P_likeRepository.java
+++ src/main/java/com/studywithus/repository/board/P_likeRepository.java
@@ -1,7 +1,6 @@
 package com.studywithus.repository.board;
 
 import com.studywithus.domain.board.P_like;
-import com.studywithus.domain.board.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
