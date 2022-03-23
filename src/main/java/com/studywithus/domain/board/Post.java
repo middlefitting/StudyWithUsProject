f4350b5411ccdfd2811af94c0018f8824be606c6
uk <uk@naver.com>
2022-03-23 17:57:28 +0900

    CHORE / STYLE: Revise 'category' field type in 'Post.java' / Revise 'Post.java'

--- src/main/java/com/studywithus/domain/board/Post.java
+++ src/main/java/com/studywithus/domain/board/Post.java
@@ -2,6 +2,7 @@ package com.studywithus.domain.board;
 
 import com.studywithus.domain.BaseEntity;
 import com.studywithus.domain.member.Member;
+
 import lombok.*;
 
 import javax.persistence.*;
@@ -9,26 +10,27 @@ import javax.persistence.*;
 @Entity
 @Getter
 @Builder
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
 @ToString
 public class Post extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long post_id; // 게시글 아이디(pk)
+    private Long post_id;
 
     @ManyToOne
-    private Member mem_id; // 작성자 아이디(fk)
+    @JoinColumn(name = "mem_id")
+    private Member mem_id;
+
+    private Category category;
 
     @Column(length = 50)
-    private String title; // 게시글 제목
+    private String title;
 
-    private String content; // 게시글 내용
+    private String content;
 
-    private String file_dir; // 첨부 파일 경로
-
-    private String category; // 카테고리
+    private String file_dir;
 
     @Column(columnDefinition = "varchar2(4000) default '0'")
-    private Integer views; // 조회수
+    private Integer views;
 }
