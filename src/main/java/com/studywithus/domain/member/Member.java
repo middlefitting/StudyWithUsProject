9382b1cd1eaaad4b855709d92c92cb5c8213cf44
middlefitting <wjdtmdcjf2@gmail.com>
2022-03-23 23:51:44 +0900

    controllerTest

--- src/main/java/com/studywithus/domain/member/Member.java
+++ src/main/java/com/studywithus/domain/member/Member.java
@@ -1,12 +1,17 @@
 package com.studywithus.domain.member;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.Setter;
+import org.apache.tomcat.jni.Local;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
 
 @Entity
 @Getter @Setter
@@ -21,19 +26,20 @@ public class Member {
 
     @Column(unique = true)
     private String nickname;
-
     private String password;
+
+//    @DateTimeFormat(pattern = "yyyy-MM-dd")
+//    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
     private String bornDate;
 
     public Member() {
     }
 
-    public Member Member(Member member){
+    public void UpdateMember(Member member){
         this.email = member.getEmail();
         this.nickname = member.getNickname();
         this.password = member.getPassword();
         this.bornDate = member.getBornDate();
-        return member;
     }
 
     public Member(String email, String nickname, String password, String bornDate) {
--- src/main/java/com/studywithus/service/member/MemberService.java
+++ src/main/java/com/studywithus/service/member/MemberService.java
@@ -1,10 +1,14 @@
 package com.studywithus.service.member;
 
 import com.studywithus.domain.member.Member;
+import com.studywithus.dto.member.CreateMemberRequestDto;
 import com.studywithus.repository.member.MemberRepository;
+import com.sun.istack.Nullable;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -12,15 +16,16 @@ import org.springframework.transaction.annotation.Transactional;
 public class MemberService {
     private final MemberRepository memberRepository;
 
-    public Long join(Member member){
+    public Long join(CreateMemberRequestDto requestDto){
+        Member member = new Member(requestDto.getEmail(), requestDto.getNickname(), requestDto.getPassword(), requestDto.getBornDate());
         memberRepository.save(member);
         return member.getId();
     }
 
-//    public void update(Long id, Member updateMember){
-//        Member member = memberRepository.findById(id);
-//        member.Member(updateMember);
-//    }
+    public void update(Long id, Member updateMember){
+        Optional<Member> optionalMember = memberRepository.findById(id);
+        optionalMember.get().UpdateMember(updateMember);
+    }
 
 
 }
