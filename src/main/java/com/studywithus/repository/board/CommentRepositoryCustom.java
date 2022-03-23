06fa52b5eb2f895e672ff128b53837a91132d58a
uk <uk@naver.com>
2022-03-23 21:48:17 +0900

    Add Repository, RepositoryCustom, RepositoryImpl

---
+++ src/main/java/com/studywithus/repository/board/CommentRepositoryCustom.java
@@ -0,0 +1,5 @@
+package com.studywithus.repository.board;
+
+public interface CommentRepositoryCustom {
+    // 구현할 메서드 명세 작성
+}
---
+++ src/main/java/com/studywithus/repository/board/CommentRepositoryImpl.java
@@ -0,0 +1,14 @@
+package com.studywithus.repository.board;
+
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+
+public class CommentRepositoryImpl extends QuerydslRepositorySupport implements CommentRepositoryCustom {
+    /**
+     * Creates a new {@link QuerydslRepositorySupport} instance for the given domain type.
+     *
+     * @param domainClass must not be {@literal null}.
+     */
+    public CommentRepositoryImpl(Class<?> domainClass) {
+        super(domainClass);
+    }
+}
