af72d5ff28ef60b9dcb7856b21aa9d974439df4b
uk <uk@naver.com>
2022-03-24 12:33:01 +0900

    CHORE: Revise class name 'repository.board', 'repository.study'

--- src/main/java/com/studywithus/repository/board/CommentRepositoryCustomImpl.java
+++ src/main/java/com/studywithus/repository/board/CommentRepositoryCustomImpl.java
@@ -2,13 +2,13 @@ package com.studywithus.repository.board;
 
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 
-public class CommentRepositoryImpl extends QuerydslRepositorySupport implements CommentRepositoryCustom {
+public class CommentRepositoryCustomImpl extends QuerydslRepositorySupport implements CommentRepositoryCustom {
     /**
      * Creates a new {@link QuerydslRepositorySupport} instance for the given domain type.
      *
      * @param domainClass must not be {@literal null}.
      */
-    public CommentRepositoryImpl(Class<?> domainClass) {
+    public CommentRepositoryCustomImpl(Class<?> domainClass) {
         super(domainClass);
     }
 }
--- src/main/java/com/studywithus/repository/board/P_likeRepositoryCustomImpl.java
+++ src/main/java/com/studywithus/repository/board/P_likeRepositoryCustomImpl.java
@@ -1,14 +1,14 @@
-package com.studywithus.repository.study;
+package com.studywithus.repository.board;
 
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 
-public class StudyRepositoryImpl extends QuerydslRepositorySupport implements StudyRepositoryCustom {
+public class P_likeRepositoryCustomImpl extends QuerydslRepositorySupport implements P_likeRepositoryCustom {
     /**
      * Creates a new {@link QuerydslRepositorySupport} instance for the given domain type.
      *
      * @param domainClass must not be {@literal null}.
      */
-    public StudyRepositoryImpl(Class<?> domainClass) {
+    public P_likeRepositoryCustomImpl(Class<?> domainClass) {
         super(domainClass);
     }
 }
