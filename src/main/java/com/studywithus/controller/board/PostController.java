f625bb62a0bbeb53ea3b7878cb34e71202b958d6
Clary <chaeyun97@gmail.com>
2022-03-30 17:09:37 +0900

    Remove Alanna

--- src/main/java/com/studywithus/controller/board/PostController.java
+++ src/main/java/com/studywithus/controller/board/PostController.java
@@ -2,11 +2,17 @@ package com.studywithus.controller.board;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/board/")
 @RequiredArgsConstructor
 public class PostController {
+
+    @GetMapping("/BoardList")
+    public void postList() {
+
+    }
 
 }
